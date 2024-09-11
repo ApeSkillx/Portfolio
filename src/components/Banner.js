cb3dcb4439b53b9ed76c531ebd17a5a7d871dad7
CHHAVIKANT <66697124+ApeSkillx@users.noreply.github.com>
2024-09-11 10:52:27 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,9 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const toRotate = [ "Web Developer" ];
+
+ // const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
   const period = 2000;
  
   useEffect(() => { 
